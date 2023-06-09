de34941d7525b91c4468b3180994edaec7a7f3ea
LiQianCA <qianjohnny@gmail.com>
2023-06-08 22:25:24 -0400

    hotelrooms update

--- app/routes/hotelbooking.routes.js
+++ app/routes/hotelbooking.routes.js
@@ -27,20 +27,4 @@ module.exports = app => {
 
 
 
-// Create a new booking
-router.post("/", booking.create);
-
-// Retrieve all bookings
-router.get("/", booking.findBooking);
-
-// Retrieve a single booking by ID
-router.get("/:bookingId", booking.findOne);
-
-// Update a booking by ID
-router.put("/:bookingId", booking.update);
-
-// Delete a booking by ID
-router.delete("/:bookingId", booking.delete);
-
-app.use('/api/bookings', router);
 };
